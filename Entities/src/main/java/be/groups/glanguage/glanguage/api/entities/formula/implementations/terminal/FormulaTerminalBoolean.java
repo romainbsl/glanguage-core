69deb25855655765acf7ee360808fde57d1cbe2d
François Dupire <francois.dupire@groups.be>
2016-09-12 13:51:21 +0200

    Fix getBooleanValue when hydrated by Hibernate

--- Entities/src/main/java/be/groups/glanguage/glanguage/api/entities/formula/implementations/terminal/FormulaTerminalBoolean.java
+++ Entities/src/main/java/be/groups/glanguage/glanguage/api/entities/formula/implementations/terminal/FormulaTerminalBoolean.java
@@ -22,46 +22,57 @@ import be.groups.glanguage.glanguage.api.entities.formula.FormulaType;
 public class FormulaTerminalBoolean extends AbstractTerminalFormula {
 
 	private Boolean booleanValue;
-	
+
 	public FormulaTerminalBoolean() {
 		super();
 	}
-	
+
 	public FormulaTerminalBoolean(String constantValue) {
 		super(constantValue);
-		if (!(constantValue.equalsIgnoreCase("true") || constantValue.equalsIgnoreCase("false"))) {
-			throw new IllegalArgumentException("Contant value must reprensent a boolean value : " + constantValue);
-		}
-		this.booleanValue = Boolean.valueOf(constantValue);
+
+		initializeBooleanValue(constantValue);
 	}
-	
+
 	public FormulaTerminalBoolean(Boolean constantValue) {
 		super(constantValue.toString());
 		this.booleanValue = Boolean.valueOf(constantValue);
 	}
-	
+
 	@Transient
 	@Override
 	public Integer getIntegerValue() {
-		throw new IllegalAccessError("Cannot invoke getIntegerValue() method on " + this.getClass().getName() + " object");
+		throw new IllegalAccessError(
+				"Cannot invoke getIntegerValue() method on " + this.getClass().getName() + " object");
 	}
-	
+
 	@Transient
 	@Override
 	public Double getNumericValue() {
-		throw new IllegalAccessError("Cannot invoke getNumericValue() method on " + this.getClass().getName() + " object");
+		throw new IllegalAccessError(
+				"Cannot invoke getNumericValue() method on " + this.getClass().getName() + " object");
 	}
-	
+
 	@Transient
 	@Override
 	public Boolean getBooleanValue() {
+		if (booleanValue == null) {
+			initializeBooleanValue(getConstantValue());
+		}
+
 		return booleanValue;
 	}
-	
+
 	@Transient
 	@Override
 	public LocalDate getDateValue() {
 		throw new IllegalAccessError("Cannot invoke getDateValue() method on " + this.getClass().getName() + " object");
 	}
-		
+
+	private void initializeBooleanValue(String constantValue) {
+		if (!(constantValue.equalsIgnoreCase("true") || constantValue.equalsIgnoreCase("false"))) {
+			throw new IllegalArgumentException("Contant value must reprensent a boolean value : " + constantValue);
+		}
+		this.booleanValue = Boolean.valueOf(constantValue);
+	}
+
 }
