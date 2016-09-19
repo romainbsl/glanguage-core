0f1eb3390955fd813e9d86d627d7dfd7b4eafb86
Romain <romain.boisselle@gmail.com>
2016-09-19 16:22:44 +0200

    test WS

--- Test/src/test/java/be/groups/glanguage/glanguage/api/ws/BaseJerseyResourceTest.java
+++ Test/src/test/java/be/groups/glanguage/glanguage/api/ws/BaseJerseyResourceTest.java
@@ -1,13 +1,32 @@
 package be.groups.glanguage.glanguage.api.ws;
 
-import be.groups.common.test.utils.WsJerseyTest;
+import be.groups.common.test.utils.Environment;
+import be.groups.marmota.test.TNSNames;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.BeforeClass;
 
-public class BaseJerseyResourceTest extends WsJerseyTest {
+public class BaseJerseyResourceTest extends JerseyTest {
 
     @Override
     protected javax.ws.rs.core.Application configure() {
         enableTestProperties();
         return new be.groups.glanguage.glanguage.api.ws.Application();
     }
-	
+
+    @BeforeClass
+    public static void setUpLyricsJersey() {
+        Environment.setUp();
+        System.setProperty("env", "dev");
+    }
+
+    protected void configureClient(ClientConfig config) {
+        config.property("jersey.config.client.httpUrlConnection.setMethodWorkaround", Boolean.valueOf(true));
+        super.configureClient(config);
+    }
+
+    protected void enableTestProperties() {
+        this.enable("jersey.config.test.logging.enable");
+        this.enable("jersey.config.test.logging.dumpEntity");
+    }
 }
--- Test/src/test/java/be/groups/glanguage/glanguage/api/ws/ParsingTest.java
+++ Test/src/test/java/be/groups/glanguage/glanguage/api/ws/ParsingTest.java
@@ -1,16 +1,16 @@
 package be.groups.glanguage.glanguage.api.ws;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 import javax.ws.rs.core.Response;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class ParsingTest extends BaseJerseyResourceTest {
 	
 	@Test
 	public void test() {
-		String formulaString = "";
+		String formulaString = "0";
 		Response response = target("/glanguage/parse/" + formulaString).request().get();
         assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
 	}
