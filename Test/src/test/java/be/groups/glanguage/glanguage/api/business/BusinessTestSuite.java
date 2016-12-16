e9ae0da28e99a8a8cda35af66e07d5ed8b795ed7
Michotte Maxime <maxime.michotte@groups.be>
2016-12-16 11:48:44 +0100

    Add tests for evaluation through evaluator

--- Test/src/test/java/be/groups/glanguage/glanguage/api/business/BusinessTestSuite.java
+++ Test/src/test/java/be/groups/glanguage/glanguage/api/business/BusinessTestSuite.java
@@ -1,5 +1,6 @@
 package be.groups.glanguage.glanguage.api.business;
 
+import be.groups.glanguage.glanguage.api.business.evaluation.PlanEvaluatorTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -15,7 +16,8 @@ import be.groups.glanguage.glanguage.api.business.universe.UniverseTest;
  * @author DUPIREFR
  */
 @RunWith(Suite.class)
-@SuiteClasses({FormulaDescriptionFactoryTest.class, ParserTest.class, PlanTest.class, UniverseTest.class})
+@SuiteClasses({FormulaDescriptionFactoryTest.class, ParserTest.class, PlanEvaluatorTest.class, PlanTest.class,
+        UniverseTest.class})
 public class BusinessTestSuite {
 
 }
