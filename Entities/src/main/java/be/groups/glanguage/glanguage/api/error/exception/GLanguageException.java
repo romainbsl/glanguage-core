266ea6435f7437efaf59aecc45653abdfcd329d0
Romain Boisselle <romain.boisselle@groups.be>
2017-05-10 14:58:33 +0200

    Change Glanguage parent class to GroupsException Allow to define a more explicit RootError Code/Message

--- Entities/src/main/java/be/groups/glanguage/glanguage/api/error/exception/GLanguageException.java
+++ Entities/src/main/java/be/groups/glanguage/glanguage/api/error/exception/GLanguageException.java
@@ -1,16 +1,27 @@
 package be.groups.glanguage.glanguage.api.error.exception;
 
 import be.groups.errorframework.core.error.InnerError;
-import be.groups.errorframework.core.exception.BusinessException;
+import be.groups.errorframework.core.error.RootError;
+import be.groups.errorframework.core.exception.GroupSException;
 
 /**
  * Created by michotte on 20/12/2016.
  */
-public class GLanguageException extends BusinessException {
+public class GLanguageException extends GroupSException {
 
-    public GLanguageException(InnerError rootInnerError) {
-        super();
-        getError().setInnerError(rootInnerError);
+    public GLanguageException(InnerError innerError) {
+        super(new GlanguageError());
+        getError().setInnerError(innerError);
     }
 
+    private static class GlanguageError extends RootError {
+        @Override
+        public String getRootErrorCode() {
+            return "GLANGUAGE_ERROR";
+        }
+        @Override
+        public String getRootErrorMessage() {
+            return "Something went wrong in the process of the Glanguage. Please refer to the InnerError(s)";
+        }
+    }
 }
