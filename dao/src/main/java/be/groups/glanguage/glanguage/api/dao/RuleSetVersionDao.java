61df979c1150d2e52242fbc1fd5dba3bdf4180c9
Romain Boisselle <romain.boisselle@groups.be>
2017-05-15 14:26:22 +0200

    fix RuleSetDao queries with TemporalType converter

--- dao/src/main/java/be/groups/glanguage/glanguage/api/dao/RuleSetVersionDao.java
+++ dao/src/main/java/be/groups/glanguage/glanguage/api/dao/RuleSetVersionDao.java
@@ -4,20 +4,21 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TemporalType;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Root;
 
-import be.groups.common.persistence.base.BaseDao;
 import be.groups.glanguage.glanguage.api.entities.ruleset.RuleSet;
 import be.groups.glanguage.glanguage.api.entities.ruleset.RuleSetVersion;
 import be.groups.glanguage.glanguage.api.entities.ruleset.RuleSetVersion_;
 import be.groups.glanguage.glanguage.api.entities.ruleset.RuleSet_;
 import be.groups.marmota.persistence.JpaUtil;
+import be.groups.presta.backoffice.persistence.base.BaseDao;
 
 public class RuleSetVersionDao extends BaseDao<Integer, RuleSetVersion> {
-	
+
 	public RuleSetVersionDao() {
 		super(RuleSetVersion.class);
 	}
@@ -29,7 +30,7 @@ public class RuleSetVersionDao extends BaseDao<Integer, RuleSetVersion> {
 
 	/**
 	 * Find a {@link RuleSetVersion} by id
-	 * 
+	 *
 	 * @param ruleSetVersionId
 	 * @return The {@link RuleSetVersion} identified by {@code id}
 	 */
@@ -38,10 +39,10 @@ public class RuleSetVersionDao extends BaseDao<Integer, RuleSetVersion> {
 		ruleSetVersion.setId(ruleSetVersionId);
 		return find(ruleSetVersion);
 	}
-	
+
 	/**
 	 * Find a {@link RuleSetVersion} by rule set id and version
-	 * 
+	 *
 	 * @param ruleSetId
 	 * @param version
 	 * @return The {@link RuleSetVersion} of the {@link RuleSet} identified by {@code rulSetId} corresponding to the {@code version}
@@ -53,15 +54,15 @@ public class RuleSetVersionDao extends BaseDao<Integer, RuleSetVersion> {
 		criteria.select(entityRoot);
 		// join with rule set
 		Join<RuleSetVersion, RuleSet> ruleSet = entityRoot.join(RuleSetVersion_.ruleSet);
-		
+
 		criteria.where(builder.equal(ruleSet.get(RuleSet_.id), ruleSetId),
 				builder.equal(entityRoot.get(RuleSetVersion_.version), version));
 		return getEntityManager().createQuery(criteria).getSingleResult();
 	}
-	
+
 	/**
 	 * Find a {@link RuleSetVersion} by rule set alias and version
-	 * 
+	 *
 	 * @param ruleSetAlias
 	 * @param version
 	 * @return The {@link RuleSetVersion} of the {@link RuleSet} identified by {@code ruleSetAlias} corresponding to the {@code version}
@@ -73,17 +74,17 @@ public class RuleSetVersionDao extends BaseDao<Integer, RuleSetVersion> {
 		criteria.select(entityRoot);
 		// join with rule set
 		Join<RuleSetVersion, RuleSet> ruleSet = entityRoot.join(RuleSetVersion_.ruleSet);
-		
+
 		criteria.where(builder.or(builder.equal(ruleSet.get(RuleSet_.aliasFr), ruleSetAlias),
 				builder.equal(ruleSet.get(RuleSet_.aliasNl), ruleSetAlias), builder.equal(ruleSet.get(RuleSet_.aliasDe), ruleSetAlias),
 				builder.equal(ruleSet.get(RuleSet_.aliasX), ruleSetAlias)),
 				builder.equal(entityRoot.get(RuleSetVersion_.version), version));
 		return getEntityManager().createQuery(criteria).getSingleResult();
 	}
-	
+
 	/**
 	 * Find a {@link RuleSetVersion} by rule set id and exploitation date
-	 * 
+	 *
 	 * @param ruleSetId
 	 * @param exploitationDate
 	 * @return The {@link RuleSetVersion} of the {@link RuleSet} identified by {@code rulSetId} corresponding to the {@code exploitationDate}
@@ -91,19 +92,23 @@ public class RuleSetVersionDao extends BaseDao<Integer, RuleSetVersion> {
 	public RuleSetVersion findByRuleSetIdAndExploitationDate(Integer ruleSetId, LocalDateTime exploitationDate) {
 		// @formatter:off
 		String query = "select rsv from RuleSetVersion rsv"
-				+ " where rsv.ruleSet.id = " + ruleSetId
+				+ " where rsv.ruleSet.id = :ruleSetId "
 				+ " and rsv.exploitationStartDate = ("
 				+ " select max(rsv2.exploitationStartDate) from RuleSetVersion rsv2"
 				+ " where rsv2.ruleSet.id = rsv.ruleSet.id"
-				+ " and rsv2.exploitationStartDate < '" + exploitationDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")) + "'"
+				+ " and rsv2.exploitationStartDate < :exploitationDate "
 				+ ")";
 		// @formatter:on
-		return (RuleSetVersion) getEntityManager().createQuery(query).getSingleResult();
+		return (RuleSetVersion) getEntityManager()
+				.createQuery(query)
+				.setParameter("ruleSetId", ruleSetId)
+				.setParameter("exploitationDate", exploitationDate)
+				.getSingleResult();
 	}
-	
+
 	/**
 	 * Find a {@link RuleSetVersion} by rule set alias and exploitation date
-	 * 
+	 *
 	 * @param ruleSetAlias
 	 * @param exploitationDate
 	 * @return The {@link RuleSetVersion} of the {@link RuleSet} identified by {@code ruleSetAlias} corresponding to the {@code exploitationDate}
@@ -111,17 +116,21 @@ public class RuleSetVersionDao extends BaseDao<Integer, RuleSetVersion> {
 	public RuleSetVersion findByRuleSetAliasAndExploitationDate(String ruleSetAlias, LocalDateTime exploitationDate) {
 		// @formatter:off
 		String query = "select rsv from RuleSetVersion rsv"
-				+ " where (rsv.ruleSet.aliasFr = '" + ruleSetAlias + "'"
-							+ " or rsv.ruleSet.aliasNl = '" + ruleSetAlias + "'" 
-							+ " or rsv.ruleSet.aliasDe = '" + ruleSetAlias + "'" 
-							+ " or rsv.ruleSet.aliasX = '" + ruleSetAlias + "')"
+				+ " where (rsv.ruleSet.aliasFr = :ruleSetAlias "
+				+ " or rsv.ruleSet.aliasNl = :ruleSetAlias "
+				+ " or rsv.ruleSet.aliasDe = :ruleSetAlias "
+				+ " or rsv.ruleSet.aliasX = :ruleSetAlias) "
 				+ " and rsv.exploitationStartDate = ("
 				+ " select max(rsv2.exploitationStartDate) from RuleSetVersion rsv2"
 				+ " where rsv2.ruleSet.id = rsv.ruleSet.id"
-				+ " and rsv2.exploitationStartDate < '" + exploitationDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")) + "'"
+				+ " and rsv2.exploitationStartDate < :exploitationDate "
 				+ ")";
 		// @formatter:on
-		return (RuleSetVersion) getEntityManager().createQuery(query).getSingleResult();
+		return (RuleSetVersion) getEntityManager()
+				.createQuery(query)
+				.setParameter("ruleSetAlias", ruleSetAlias)
+				.setParameter("exploitationDate", exploitationDate)
+				.getSingleResult();
 	}
-	
+
 }
