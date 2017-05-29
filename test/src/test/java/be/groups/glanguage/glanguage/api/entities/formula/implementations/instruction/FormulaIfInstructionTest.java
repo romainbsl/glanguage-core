525c68a78c2149f6c77551f7ad2f46d95e825be6
Michotte Maxime <maxime.michotte@groups.be>
2017-05-29 09:52:50 +0200

    Adapt tests for classes in package formula.implementations.instruction

--- test/src/test/java/be/groups/glanguage/glanguage/api/entities/formula/implementations/instruction/FormulaIfInstructionTest.java
+++ test/src/test/java/be/groups/glanguage/glanguage/api/entities/formula/implementations/instruction/FormulaIfInstructionTest.java
@@ -8,11 +8,11 @@ import org.junit.Test;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
  * Test class for {@link FormulaIfInstruction}
@@ -61,7 +61,8 @@ public class FormulaIfInstructionTest {
 		when(elseStatement.getReturnType(null)).thenReturn(FormulaReturnType.INTEGER);
 		when(elseStatement.getIntegerValue(null)).thenReturn(1);
 		
-		FormulaIfInstruction formula = new FormulaIfInstruction(null, condition, ifStatement, elseStatement);
+		FormulaIfInstruction formula = spy(FormulaIfInstruction.class);
+		doReturn(Arrays.asList(condition, ifStatement, elseStatement)).when(formula).getParameters();
 		
 		assertEquals(Integer.valueOf(2), formula.getIntegerValue(null));
 	}
@@ -82,9 +83,10 @@ public class FormulaIfInstructionTest {
 		AbstractFormula elseStatement = mock(AbstractFormula.class);
 		when(elseStatement.getReturnType(null)).thenReturn(FormulaReturnType.INTEGER);
 		when(elseStatement.getIntegerValue(null)).thenReturn(1);
-		
-		FormulaIfInstruction formula = new FormulaIfInstruction(null, condition, ifStatement, elseStatement);
-		
+
+		FormulaIfInstruction formula = spy(FormulaIfInstruction.class);
+		doReturn(Arrays.asList(condition, ifStatement, elseStatement)).when(formula).getParameters();
+
 		assertEquals(Integer.valueOf(1), formula.getIntegerValue(null));
 	}
 	
@@ -106,8 +108,9 @@ public class FormulaIfInstructionTest {
 		when(subElseStatement.getReturnType(null)).thenReturn(FormulaReturnType.STRING);
 		when(subElseStatement.getIntegerValue(null)).thenReturn(1);
 		
-		FormulaIfInstruction subFormula = new FormulaIfInstruction(null, subCondition, subIfStatement, subElseStatement);
-		
+		FormulaIfInstruction subFormula = spy(FormulaIfInstruction.class);
+		doReturn(Arrays.asList(subCondition, subIfStatement, subElseStatement)).when(subFormula).getParameters();
+
 		AbstractFormula condition = mock(AbstractFormula.class);
 		when(condition.getReturnType(null)).thenReturn(FormulaReturnType.BOOLEAN);
 		when(condition.getBooleanValue(null)).thenReturn(false);
@@ -115,9 +118,10 @@ public class FormulaIfInstructionTest {
 		AbstractFormula ifStatement = mock(AbstractFormula.class);
 		when(ifStatement.getReturnType(null)).thenReturn(FormulaReturnType.STRING);
 		when(ifStatement.getIntegerValue(null)).thenReturn(3);
-		
-		FormulaIfInstruction formula = new FormulaIfInstruction(null, condition, ifStatement, subFormula);
-		
+
+		FormulaIfInstruction formula = spy(FormulaIfInstruction.class);
+		doReturn(Arrays.asList(condition, ifStatement, subFormula)).when(formula).getParameters();
+
 		assertEquals(Integer.valueOf(2), formula.getIntegerValue(null));
 	}
 	
@@ -137,9 +141,10 @@ public class FormulaIfInstructionTest {
 		AbstractFormula elseStatement = mock(AbstractFormula.class);
 		when(elseStatement.getReturnType(null)).thenReturn(FormulaReturnType.NUMERIC);
 		when(elseStatement.getNumericValue(null)).thenReturn(1.5);
-		
-		FormulaIfInstruction formula = new FormulaIfInstruction(null, condition, ifStatement, elseStatement);
-		
+
+		FormulaIfInstruction formula = spy(FormulaIfInstruction.class);
+		doReturn(Arrays.asList(condition, ifStatement, elseStatement)).when(formula).getParameters();
+
 		assertEquals(Double.valueOf(2.5), formula.getNumericValue(null));
 	}
 	
@@ -159,9 +164,10 @@ public class FormulaIfInstructionTest {
 		AbstractFormula elseStatement = mock(AbstractFormula.class);
 		when(elseStatement.getReturnType(null)).thenReturn(FormulaReturnType.NUMERIC);
 		when(elseStatement.getNumericValue(null)).thenReturn(1.5);
-		
-		FormulaIfInstruction formula = new FormulaIfInstruction(null, condition, ifStatement, elseStatement);
-		
+
+		FormulaIfInstruction formula = spy(FormulaIfInstruction.class);
+		doReturn(Arrays.asList(condition, ifStatement, elseStatement)).when(formula).getParameters();
+
 		assertEquals(Double.valueOf(1.5), formula.getNumericValue(null));
 	}
 	
@@ -181,9 +187,10 @@ public class FormulaIfInstructionTest {
 		AbstractFormula elseStatement = mock(AbstractFormula.class);
 		when(elseStatement.getReturnType(null)).thenReturn(FormulaReturnType.STRING);
 		when(elseStatement.getStringValue(null)).thenReturn("condIsFalse");
-		
-		FormulaIfInstruction formula = new FormulaIfInstruction(null, condition, ifStatement, elseStatement);
-		
+
+		FormulaIfInstruction formula = spy(FormulaIfInstruction.class);
+		doReturn(Arrays.asList(condition, ifStatement, elseStatement)).when(formula).getParameters();
+
 		assertEquals("condIsTrue", formula.getStringValue(null));
 	}
 	
@@ -203,9 +210,10 @@ public class FormulaIfInstructionTest {
 		AbstractFormula elseStatement = mock(AbstractFormula.class);
 		when(elseStatement.getReturnType(null)).thenReturn(FormulaReturnType.STRING);
 		when(elseStatement.getStringValue(null)).thenReturn("condIsFalse");
-		
-		FormulaIfInstruction formula = new FormulaIfInstruction(null, condition, ifStatement, elseStatement);
-		
+
+		FormulaIfInstruction formula = spy(FormulaIfInstruction.class);
+		doReturn(Arrays.asList(condition, ifStatement, elseStatement)).when(formula).getParameters();
+
 		assertEquals("condIsFalse", formula.getStringValue(null));
 	}
 	
@@ -225,9 +233,10 @@ public class FormulaIfInstructionTest {
 		AbstractFormula elseStatement = mock(AbstractFormula.class);
 		when(elseStatement.getReturnType(null)).thenReturn(FormulaReturnType.BOOLEAN);
 		when(elseStatement.getBooleanValue(null)).thenReturn(false);
-		
-		FormulaIfInstruction formula = new FormulaIfInstruction(null, condition, ifStatement, elseStatement);
-		
+
+		FormulaIfInstruction formula = spy(FormulaIfInstruction.class);
+		doReturn(Arrays.asList(condition, ifStatement, elseStatement)).when(formula).getParameters();
+
 		assertEquals(Boolean.TRUE, formula.getBooleanValue(null));
 	}
 	
@@ -247,9 +256,10 @@ public class FormulaIfInstructionTest {
 		AbstractFormula elseStatement = mock(AbstractFormula.class);
 		when(elseStatement.getReturnType(null)).thenReturn(FormulaReturnType.BOOLEAN);
 		when(elseStatement.getBooleanValue(null)).thenReturn(false);
-		
-		FormulaIfInstruction formula = new FormulaIfInstruction(null, condition, ifStatement, elseStatement);
-		
+
+		FormulaIfInstruction formula = spy(FormulaIfInstruction.class);
+		doReturn(Arrays.asList(condition, ifStatement, elseStatement)).when(formula).getParameters();
+
 		assertEquals(Boolean.FALSE, formula.getBooleanValue(null));
 	}
 	
@@ -269,9 +279,10 @@ public class FormulaIfInstructionTest {
 		AbstractFormula elseStatement = mock(AbstractFormula.class);
 		when(elseStatement.getReturnType(null)).thenReturn(FormulaReturnType.DATE);
 		when(elseStatement.getDateValue(null)).thenReturn(LocalDate.of(2014, 1, 1));
-		
-		FormulaIfInstruction formula = new FormulaIfInstruction(null, condition, ifStatement, elseStatement);
-		
+
+		FormulaIfInstruction formula = spy(FormulaIfInstruction.class);
+		doReturn(Arrays.asList(condition, ifStatement, elseStatement)).when(formula).getParameters();
+
 		assertEquals(LocalDate.of(2015, 1, 1), formula.getDateValue(null));
 	}
 	
@@ -291,9 +302,10 @@ public class FormulaIfInstructionTest {
 		AbstractFormula elseStatement = mock(AbstractFormula.class);
 		when(elseStatement.getReturnType(null)).thenReturn(FormulaReturnType.DATE);
 		when(elseStatement.getDateValue(null)).thenReturn(LocalDate.of(2014, 1, 1));
-		
-		FormulaIfInstruction formula = new FormulaIfInstruction(null, condition, ifStatement, elseStatement);
-		
+
+		FormulaIfInstruction formula = spy(FormulaIfInstruction.class);
+		doReturn(Arrays.asList(condition, ifStatement, elseStatement)).when(formula).getParameters();
+
 		assertEquals(LocalDate.of(2014, 1, 1), formula.getDateValue(null));
 	}
 	
@@ -313,9 +325,10 @@ public class FormulaIfInstructionTest {
 		AbstractFormula elseStatement = mock(AbstractFormula.class);
 		when(elseStatement.getReturnType(null)).thenReturn(FormulaReturnType.DURATION);
 		when(elseStatement.getDurationValue(null)).thenReturn(Duration.ofDays(3L));
-		
-		FormulaIfInstruction formula = new FormulaIfInstruction(null, condition, ifStatement, elseStatement);
-		
+
+		FormulaIfInstruction formula = spy(FormulaIfInstruction.class);
+		doReturn(Arrays.asList(condition, ifStatement, elseStatement)).when(formula).getParameters();
+
 		assertEquals(Duration.ofDays(2L), formula.getDurationValue(null));
 	}
 	
@@ -335,9 +348,10 @@ public class FormulaIfInstructionTest {
 		AbstractFormula elseStatement = mock(AbstractFormula.class);
 		when(elseStatement.getReturnType(null)).thenReturn(FormulaReturnType.DURATION);
 		when(elseStatement.getDurationValue(null)).thenReturn(Duration.ofDays(3L));
-		
-		FormulaIfInstruction formula = new FormulaIfInstruction(null, condition, ifStatement, elseStatement);
-		
+
+		FormulaIfInstruction formula = spy(FormulaIfInstruction.class);
+		doReturn(Arrays.asList(condition, ifStatement, elseStatement)).when(formula).getParameters();
+
 		assertEquals(Duration.ofDays(3L), formula.getDurationValue(null));
 	}
 	
@@ -357,9 +371,10 @@ public class FormulaIfInstructionTest {
 		AbstractFormula elseStatement = mock(AbstractFormula.class);
 		when(elseStatement.getReturnType(null)).thenReturn(FormulaReturnType.STRING);
 		when(elseStatement.asText()).thenReturn("some_rule2");
-		
-		FormulaIfInstruction formula = new FormulaIfInstruction(null, condition, ifStatement, elseStatement);
-		
+
+		FormulaIfInstruction formula = spy(FormulaIfInstruction.class);
+		doReturn(Arrays.asList(condition, ifStatement, elseStatement)).when(formula).getParameters();
+
 		assertEquals("if some_cond then\n\tsome_rule1\nelse\n\tsome_rule2\nend", formula.asText());
 	}
 	
@@ -379,9 +394,10 @@ public class FormulaIfInstructionTest {
 		AbstractFormula subElseStatement = mock(AbstractFormula.class);
 		when(subElseStatement.getReturnType(null)).thenReturn(FormulaReturnType.STRING);
 		when(subElseStatement.asText()).thenReturn("some_rule3");
-		
-		FormulaIfInstruction subFormula = new FormulaIfInstruction(null, subCondition, subIfStatement, subElseStatement);
-		
+
+		FormulaIfInstruction subFormula = spy(FormulaIfInstruction.class);
+		doReturn(Arrays.asList(subCondition, subIfStatement, subElseStatement)).when(subFormula).getParameters();
+
 		AbstractFormula condition = mock(AbstractFormula.class);
 		when(condition.getReturnType(null)).thenReturn(FormulaReturnType.BOOLEAN);
 		when(condition.asText()).thenReturn("some_cond1");
@@ -389,9 +405,10 @@ public class FormulaIfInstructionTest {
 		AbstractFormula ifStatement = mock(AbstractFormula.class);
 		when(ifStatement.getReturnType(null)).thenReturn(FormulaReturnType.STRING);
 		when(ifStatement.asText()).thenReturn("some_rule1");
-		
-		FormulaIfInstruction formula = new FormulaIfInstruction(null, condition, ifStatement, subFormula);
-		
+
+		FormulaIfInstruction formula = spy(FormulaIfInstruction.class);
+		doReturn(Arrays.asList(condition, ifStatement, subFormula)).when(formula).getParameters();
+
 		assertEquals("if some_cond1 then\n\tsome_rule1\nelseif some_cond2 then\n\tsome_rule2\nelse\n\tsome_rule3\nend",
 				formula.asText());
 	}
