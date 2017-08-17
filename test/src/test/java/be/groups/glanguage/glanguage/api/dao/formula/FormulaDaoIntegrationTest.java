76da114ed0c4efeba689b9bc1c2cb49ae16a2c84
Romain Boisselle <romain.boisselle@groups.be>
2017-08-17 13:52:13 +0200

    update FormulaDaoIntegrationTest

--- test/src/test/java/be/groups/glanguage/glanguage/api/dao/formula/FormulaDaoIntegrationTest.java
+++ test/src/test/java/be/groups/glanguage/glanguage/api/dao/formula/FormulaDaoIntegrationTest.java
@@ -2,7 +2,9 @@ package be.groups.glanguage.glanguage.api.dao.formula;
 
 import be.groups.glanguage.glanguage.api.dao.*;
 import be.groups.glanguage.glanguage.api.entities.formula.*;
+import be.groups.glanguage.glanguage.api.test.categories.*;
 import org.junit.*;
+import org.junit.experimental.categories.*;
 import org.junit.runner.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.test.context.junit4.*;
@@ -20,5 +22,26 @@ public class FormulaDaoIntegrationTest extends DaoIntegrationTest {
     AbstractFormula formula = formulaDao.findById(-900000);
     assertNotNull(formula);
     assertEquals(-900000, formula.getId());
+  }
+
+  /**
+   * Tests {@link AbstractFormula} JPA mapping
+   */
+  @Test
+  @Category(JpaMappingTestsCategory.class)
+  public void testJpaMapping() {
+    AbstractFormula formula = formulaDao.findById(-900003);
+
+		/* Checking entity */
+    assertNotNull(formula);
+
+    assertEquals(-900003, formula.getId());
+
+    assertEquals("TRUE", formula.getConstantValue());
+    assertEquals(Integer.valueOf(4), formula.getSequenceNumber());
+
+		/* Checking relationships */
+    assertNotNull(formula.getDescription());
+    assertEquals(Integer.valueOf(1004), formula.getDescription().getId());
   }
 }
