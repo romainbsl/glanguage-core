c7ff7f46755867d337a28dc99aec0d7557f22b2e
Romain Boisselle <romain.boisselle@groups.be>
2017-08-21 14:53:41 +0200

    remove deprecated function from RuleIdentityDao

--- dao/src/main/java/be/groups/glanguage/glanguage/api/dao/rule/RuleIdentityDao.java
+++ dao/src/main/java/be/groups/glanguage/glanguage/api/dao/rule/RuleIdentityDao.java
@@ -2,26 +2,10 @@ package be.groups.glanguage.glanguage.api.dao.rule;
 
 import be.groups.glanguage.glanguage.api.entities.rule.*;
 import org.springframework.data.jpa.repository.*;
-import org.springframework.data.repository.query.*;
 import org.springframework.stereotype.*;
 
 /**
  * DAO for {@link RuleIdentity} <p>
  */
 @Repository
-public interface RuleIdentityDao extends JpaRepository<RuleIdentity, Integer> {
-
-  /**
-   * Find a {@link RuleIdentity} by id by {@code ruleSetVersionId} with all his children
-   *
-   * @param ruleIdentityId the identifier of the {@link RuleIdentity} to be returned
-   * @return The {@link RuleIdentity} identified by {@code id} with all his children, {@Link
-   * RuleIdentity#ruleDefinitions}, or null if it doesn't exists
-   */
-  @Deprecated // TODO must use super.findById() in a @Transactional state
-  @Query("Select ri from RuleIdentity ri "
-      + " join fetch ri.ruleDefinitions rd "
-      + " where ri.id = :ruleIdentityId "
-      + " and rd.ruleIdentity = ri ")
-  RuleIdentity findEagerById(@Param("ruleIdentityId") Integer ruleIdentityId);
-}
+public interface RuleIdentityDao extends JpaRepository<RuleIdentity, Integer> {}
